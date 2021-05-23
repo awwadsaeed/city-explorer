452f5f069708059cab52ac7f7e7a62086e41e21e
awwadsaeed <saeedawwad450@gmail.com>
2021-05-23 18:33:56 +0300

    first version created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import './App.css';
 import React from 'react';
+import FormInfo from './Form.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import InfoCard from './Card'
 
-class App extends React.Component{
-render(){
-  return(
-    <>
-    <form>
-      <input type='submit' value='enter city name'/>
-    </form>
-    </>
-  )
-}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataInfo: '',
+      show:false,
+      display:false,
+    }
+  }
+
+  setData =(data,showing) => {
+   this.setState({
+      dataInfo: data,
+      show:true,
+      display:showing
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <FormInfo setData={this.setData} />
+        {this.state.show &&
+        <InfoCard data={this.state.dataInfo} display={this.state.display} />
+        }
+      </>
+    )
+  }
 }
 
 export default App;
